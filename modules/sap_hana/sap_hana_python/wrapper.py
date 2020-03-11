46feb420269796be7877d3a14de24c67f5f46aac
Alfonso Palacios <alpalacios@google.com>
2020-03-11 08:47:18 +0100

    Fixing python formatting

--- modules/sap_hana/sap_hana_python/wrapper.py
+++ modules/sap_hana/sap_hana_python/wrapper.py
@@ -1,29 +1,32 @@
 import sap_hana
 import sys
 
+
+
+
 class Context:
-  def __init__(self, instance_type):
-    self.properties = {
-      "zone": "",
-      "subnetwork": "",
-      "publicIP": "",
-      "instanceName": "",
-      "instanceType": instance_type,
-      "linuxImage": "",
-      "linuxImageProject": "",
-      "sap_hana_scaleout_nodes": 0,
-    }
-    self.env = {
-      "project": "",
-      "project_number": "",
-    }
+    def __init__(self, instance_type):
+        self.properties = {
+            "zone": "",
+            "subnetwork": "",
+            "publicIP": "",
+            "instanceName": "",
+            "instanceType": instance_type,
+            "linuxImage": "",
+            "linuxImageProject": "",
+            "sap_hana_scaleout_nodes": 0,
+        }
+        self.env = {
+            "project": "",
+            "project_number": "",
+        }
 
 
 if __name__ == '__main__':
-  instance_type = sys.argv[1]
+    instance_type = sys.argv[1]
 
-  context = Context(instance_type)
+    context = Context(instance_type)
 
-  out = sap_hana.GenerateConfig(context)
+    out = sap_hana.GenerateConfig(context)
 
-  print(out['resources'][0]['properties']['sizeGb'])
+    print(out['resources'][0]['properties']['sizeGb'])
