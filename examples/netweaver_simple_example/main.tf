68718c3c983caaaef913b59d766db9cdb1912d5d
Alfonso Palacios <alpalacios@google.com>
2020-03-18 17:22:34 +0100

    Revert "Updating Google Cloud provider version. We are going to need some recent features (like encryption keys in persistent disks)."
    This reverts commit 44ad3726514e446b10ea1fd7c89be5e4ac2a075c.

--- examples/netweaver_simple_example/main.tf
+++ examples/netweaver_simple_example/main.tf
@@ -15,7 +15,7 @@
  */
 
 provider "google" {
-  version = "~> 3.13.0"
+  version = "~> 2.6.0"
 }
 
 module "gcp_netweaver" {
--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,9 +19,9 @@ terraform {
 }
 
 provider "google" {
-  version = "~> 3.13.0"
+  version = "~> 2.12.0"
 }
 
 provider "google-beta" {
-  version = "~> 3.13.0"
+  version = "~> 2.12.0"
 }
