2263b154b023151855819ecab406346dae1bba02
sjswerdlow <sjswerdlow@google.com>
2022-10-31 22:19:19 +0000

    Changes prepare the repository for the new templates.

--- modules/sap_hana_ha/outputs.tf
+++ modules/sap_hana_ha/outputs.tf
@@ -1,45 +1,16 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-output "primary_instance_name" {
-  description = "Name of sap primary instance"
-  value       = google_compute_instance.primary.name
+output "sap_hana_ha_primary_instance_self_link" {
+  description = "Self-link for the primary SAP HANA HA instance created."
+  value = google_compute_instance.sap_hana_ha_primary_instance.self_link
 }
-
-output "primary_zone" {
-  description = "Compute Engine primary instance deployment zone"
-  value       = google_compute_instance.primary.zone
+output "sap_hana_ha_secondary_instance_self_link" {
+  description = "Self-link for the secondary SAP HANA HA instance created."
+  value = google_compute_instance.sap_hana_ha_secondary_instance.self_link
 }
-
-output "primary_instance_machine_type" {
-  description = "Primary GCE instance/machine type."
-  value       = google_compute_instance.primary.machine_type
+output "sap_hana_ha_loadbalander_link" {
+  description = "Link to the optional load balancer"
+  value = google_compute_region_backend_service.sap_hana_ha_loadbalancer.*.self_link
 }
-
-output "secondary_instance_name" {
-  description = "Name of sap secondary instance"
-  value       = google_compute_instance.secondary.name
-}
-
-output "secondary_zone" {
-  description = "Compute Engine secondary instance deployment zone"
-  value       = google_compute_instance.secondary.zone
-}
-
-output "secondary_instance_machine_type" {
-  description = "Secondary GCE instance/machine type."
-  value       = google_compute_instance.secondary.machine_type
+output "sap_hana_ha_firewall_link" {
+  description = "Link to the optional fire wall"
+  value = google_compute_firewall.sap_hana_ha_vpc_firewall.*.self_link
 }
