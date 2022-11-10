ab7718625dda36d40c57b71848e98511f95e65f5
Awais Malik <awmalik@google.com>
2022-11-10 00:40:35 +0000

    fixes lint issues

--- examples/sap_hana_ha_simple/main.tf
+++ examples/sap_hana_ha_simple/main.tf
@@ -26,14 +26,14 @@ module "sap_hana_ha" {
   # source = "terraform-google-modules/sap/google"
   # version = "0.5.0"
 
-  project_id                      = var.project_id
-  machine_type                    = "n1-highmem-32"
-  network                         = "default"
-  subnetwork                      = "default"
-  linux_image                     = "rhel-8-4-sap-ha"
-  linux_image_project             = "rhel-sap-cloud"
-  primary_instance_name           = "hana-ha-primary"
-  primary_zone                    = "us-east1-b"
-  secondary_instance_name         = "hana-ha-secondary"
-  secondary_zone                  = "us-east1-c"
+  project_id              = var.project_id
+  machine_type            = "n1-highmem-32"
+  network                 = "default"
+  subnetwork              = "default"
+  linux_image             = "rhel-8-4-sap-ha"
+  linux_image_project     = "rhel-sap-cloud"
+  primary_instance_name   = "hana-ha-primary"
+  primary_zone            = "us-east1-b"
+  secondary_instance_name = "hana-ha-secondary"
+  secondary_zone          = "us-east1-c"
 }
